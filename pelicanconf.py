a97bb7bb515cb17044985b93f5141e7c1da7f857
Yann van Ewijk <yannvanewijk@gmail.com>
2017-01-09 22:19:40 +0100

    Third first post.

---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Yann van Ewijk'
+SITENAME = 'Technoprenerd'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Amsterdam'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'pelican-clean-blog'
+COLOR_SCHEME_CSS = 'darkly.css'
+
+#HEADER_COLOR = 'black'
+HEADER_COVER = 'static/cyberpunk-1.jpg'
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/yannvanewijk'),
+          ('twitter', 'https://twitter.com/technoprenerd'),)
+
+DEFAULT_PAGINATION = 25
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
